1d1652ca8b1cc2e4ce16cf0dcbd4083038d7c6d9
atarbabgei <atarbabgei@gmail.com>
2024-09-04 19:05:25 +0100

    add key_mappings config

--- keyboard_joy/joy_node.py
+++ keyboard_joy/joy_node.py
@@ -5,6 +5,9 @@ from rclpy.node import Node
 from sensor_msgs.msg import Joy
 from pynput import keyboard
 import threading
+import yaml
+import os
+from ament_index_python.packages import get_package_share_directory
 
 class KeyboardJoy(Node):
     def __init__(self):
@@ -24,6 +27,9 @@ class KeyboardJoy(Node):
         # Create a lock for thread-safe updates
         self.lock = threading.Lock()
 
+        # Load key mappings from YAML file installed with the package
+        self.load_key_mappings()
+
         # Start a thread to listen to keyboard inputs
         self.listener_thread = threading.Thread(target=self.start_keyboard_listener)
         self.listener_thread.start()
@@ -41,6 +47,19 @@ class KeyboardJoy(Node):
         self.get_logger().info("  Arrow Left/Right: Increase/Decrease axis 4 value")
         self.get_logger().info("  0-9: Press number keys to toggle corresponding button")
 
+    def load_key_mappings(self):
+        """Load key mappings from a YAML file installed with the package."""
+        # Get the path to the package share directory
+        config_file_path = os.path.join(get_package_share_directory('keyboard_joy'), 'config', 'key_mappings.yaml')
+        
+        # Load the YAML file
+        with open(config_file_path, 'r') as file:
+            key_mappings = yaml.safe_load(file)
+
+        # Extract axes and buttons mappings from the loaded YAML file
+        self.axis_mappings = key_mappings['axes']
+        self.button_mappings = key_mappings['buttons']
+
     def start_keyboard_listener(self):
         """Start listening to keyboard inputs in a separate thread."""
         with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as listener:
@@ -49,46 +68,34 @@ class KeyboardJoy(Node):
     def on_press(self, key):
         """Callback for keyboard key press events."""
         with self.lock:
-            try:
-                # Adjust axes based on key presses
-                if key.char == 'w':
-                    self.joy_msg.axes[0] = 1.0  # Increase axis 1
-                elif key.char == 's':
-                    self.joy_msg.axes[0] = -1.0  # Decrease axis 1
-                elif key.char == 'd':
-                    self.joy_msg.axes[1] = 1.0  # Increase axis 2
-                elif key.char == 'a':
-                    self.joy_msg.axes[1] = -1.0  # Decrease axis 2
-                elif key.char in '0123456789':
-                    self.joy_msg.buttons[int(key.char)] = 1  # Toggle button state
-            except AttributeError:
-                # Use special keys for more axes
-                if key == keyboard.Key.up:
-                    self.joy_msg.axes[2] = 1.0  # Increase axis 3
-                elif key == keyboard.Key.down:
-                    self.joy_msg.axes[2] = -1.0  # Decrease axis 3
-                elif key == keyboard.Key.left:
-                    self.joy_msg.axes[3] = -1.0  # Decrease axis 4
-                elif key == keyboard.Key.right:
-                    self.joy_msg.axes[3] = 1.0  # Increase axis 4
+            key_str = self.key_to_string(key)
+            if key_str in self.axis_mappings:
+                axis, value = self.axis_mappings[key_str]
+                self.joy_msg.axes[axis] = value
+            elif key_str in self.button_mappings:
+                button_index = self.button_mappings[key_str]
+                self.joy_msg.buttons[button_index] = 1
 
     def on_release(self, key):
         """Callback for keyboard key release events."""
         with self.lock:
-            try:
-                # Reset axes when keys are released
-                if key.char in ['w', 's']:
-                    self.joy_msg.axes[0] = 0.0
-                elif key.char in ['a', 'd']:
-                    self.joy_msg.axes[1] = 0.0
-                elif key.char in '0123456789':
-                    self.joy_msg.buttons[int(key.char)] = 0
-            except AttributeError:
-                # Reset axes for special keys
-                if key in [keyboard.Key.up, keyboard.Key.down]:
-                    self.joy_msg.axes[2] = 0.0
-                elif key in [keyboard.Key.left, keyboard.Key.right]:
-                    self.joy_msg.axes[3] = 0.0
+            key_str = self.key_to_string(key)
+            if key_str in self.axis_mappings:
+                axis, _ = self.axis_mappings[key_str]
+                self.joy_msg.axes[axis] = 0.0
+            elif key_str in self.button_mappings:
+                button_index = self.button_mappings[key_str]
+                self.joy_msg.buttons[button_index] = 0
+
+    def key_to_string(self, key):
+        """Convert keyboard key to string for parameter lookup."""
+        if hasattr(key, 'char') and key.char is not None:
+            return key.char
+        elif hasattr(key, 'name') and key.name is not None:
+            return f'Key.{key.name}'
+        else:
+            # Fallback to the string representation if all else fails
+            return str(key)
 
     def publish_joy(self):
         """Publish the Joy message based on current keyboard state."""
